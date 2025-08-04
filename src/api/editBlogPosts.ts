647bb9d1dd028b61c22e79ff9d206a38ceb99f59
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-08-04 01:16:05 +0100

    implemented editBlogPosts function

---
+++ src/api/editBlogPosts.ts
@@ -0,0 +1,35 @@
+import { Alert } from "react-native";
+import jsonServer from "./jsonServer";
+
+interface editBlogProps {
+	newTitle: string | undefined;
+	newContent: string | undefined;
+	id: number | undefined;
+}
+
+export const editBlogPosts = async ({
+	newTitle,
+	newContent,
+	id,
+}: editBlogProps) => {
+	console.log("EDITING POST:", id, newTitle, newContent);
+	console.log(typeof id);
+
+	try {
+		const response = await jsonServer.patch(`/blogPosts/:${id}`, {
+			title: newTitle,
+			content: newContent,
+		});
+
+		if (!response || !response.data)
+			throw new Error("Blog could not be edited");
+
+		return response.data;
+	} catch (err) {
+		if (err instanceof Error) {
+			Alert.alert("Error", err.message);
+		} else {
+			Alert.alert("Error", "Something went wrong");
+		}
+	}
+};

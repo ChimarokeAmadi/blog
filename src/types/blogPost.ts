fbe621161b6fd8890494856d62352affb77175b7
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-08-04 01:23:59 +0100

    added disabled tp BlogFormProps type

--- src/types/blogPost.ts
+++ src/types/blogPost.ts
@@ -2,5 +2,6 @@ export type BlogFormProps = {
 	buttonTitle: string;
 	initialTitle?: string | undefined;
 	initialContent?: string | undefined;
+	disabled?: boolean;
 	onSubmit: (title: string, content: string) => void;
 };

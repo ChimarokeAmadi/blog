16fad34a15190a55c141d84b8a8c6700b19f4f8a
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-08-04 01:19:25 +0100

    added create and edit mutate functions to BlogPostContext file

--- src/context/BlogPostContext.tsx
+++ src/context/BlogPostContext.tsx
@@ -1,13 +1,27 @@
 import { useContext, createContext, Children } from "react";
 import { fetchBlogPosts } from "../api/fetchBlogPosts";
-import { State } from "../types";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { BlogPost, State } from "../types";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { CreateBlogPosts } from "../api/createBlogPosts";
+import { editBlogPosts } from "../api/editBlogPosts";
 
 interface BlogPostContextType {
 	blogPosts: State;
 	isPending: boolean;
 	error: unknown;
 	refetchBlogPosts: () => void;
+	createPost: ({ title, content }: { title: string; content: string }) => void;
+	isCreating: boolean;
+	editPost: ({
+		newTitle,
+		newContent,
+		id,
+	}: {
+		newTitle: string | undefined;
+		newContent: string | undefined;
+		id: number | undefined;
+	}) => void;
+	isEditing: boolean;
 }
 
 const BlogPostContext = createContext<BlogPostContextType | undefined>(
@@ -20,6 +34,7 @@ export const BlogPostProvider = ({
 	children: React.ReactNode;
 }) => {
 	const queryContext = useQueryClient();
+
 	const {
 		data: blogPosts,
 		isPending,
@@ -30,11 +45,33 @@ export const BlogPostProvider = ({
 		queryFn: fetchBlogPosts,
 	});
 
+	const { mutate: createPost, isPending: isCreating } = useMutation({
+		mutationFn: CreateBlogPosts,
+		onSuccess: () => {
+			queryContext.invalidateQueries({
+				queryKey: ["blogPosts"],
+			});
+		},
+	});
+
+	const { mutate: editPost, isPending: isEditing } = useMutation({
+		mutationFn: editBlogPosts,
+		onSuccess: () => {
+			queryContext.invalidateQueries({
+				queryKey: ["blogPosts"],
+			});
+		},
+	});
+
 	const value: BlogPostContextType = {
 		blogPosts,
 		isPending,
 		error,
 		refetchBlogPosts,
+		createPost,
+		isCreating,
+		editPost,
+		isEditing,
 	};
 
 	return (
